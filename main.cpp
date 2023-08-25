9f90d7c814e7968442b1fe73067b8277629265aa
Kyu Lee <kwooler@gmail.com>
2023-08-24 20:46:18 -0700

    A35

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
-  const double ClassACost = 15.00;
-  const double ClassBCost = 12.00;
-  const double ClassCCost = 9.00;
+  int rdnum1, rdnum2, rdnum3;
+  int total = 0;
 
   /* ************************************
     Code your program here
   ************************************ */
 
   cout << setprecision(2) << fixed;
-  cout << "Sales Price for A: " << totalA << endl;
-  cout << "Sales Price for B: " << totalB << endl;
-  cout << "Sales Price for C: " << totalC << endl;
-  cout << "Total Sales: " << total << endl;
+  cout << "Random values are " << rdnum1 << "\t" << rdnum2 << "\t" << rdnum3 << endl;
+  cout << "Total summation: " << total << "\t"
+       << "Average "
+       << "\t" << total / 3 << endl;
 }
---
+++ main_test.py
@@ -0,0 +1,32 @@
+import io
+import sys
+import re
+
+
+def test_main_1():
+    numbers = []
+    totalavg = []
+    with open('out.txt') as file:
+        for i, f in enumerate(file):
+            if i == 0:
+                print (f.rstrip('\n'))
+                res = re.split(r'\s', f)
+                for v in res:
+                    if v.isnumeric():
+                        numbers.append(int(v))
+                print (numbers)
+            elif i == 1:
+                print (f.rstrip('\n'))
+                res = re.split(r'\s', f)
+                for v in res:
+                    if v.isnumeric():
+                        totalavg.append(int(v))
+                print (totalavg)
+
+    expectedtotal = sum(numbers)
+    expectedavg = sum(numbers) / len(numbers)
+
+    assert expectedtotal == totalavg[0]
+    assert expectedavg == totalavg[1]
+
+            
