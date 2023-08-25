dd101a5a0415b41631ae1ffc22e2e9832e82c68d
Kyuwoong Lee <kwooler@gmail.com>
2023-08-24 19:57:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<!--
+## ![A6-1](https://nimbus-screenshots.s3.amazonaws.com/s/31c59c7c689afb721fa60bf9522d57bc.png) -->
+
+## Edit a file "main.cpp"
+
+> Complete the program
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  const double ClassACost = 15.00;
+  const double ClassBCost = 12.00;
+  const double ClassCCost = 9.00;
+
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Sales Price for A: " << totalA << endl;
+  cout << "Sales Price for B: " << totalB << endl;
+  cout << "Sales Price for C: " << totalC << endl;
+  cout << "Total Sales: " << total << endl;
+}
