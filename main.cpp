5d5c7ef63c41415684f789a0bedaeab9a95e9ed1
seazen <158005067+seazensapkota@users.noreply.github.com>
2024-03-15 01:42:30 +0000

    3-5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main()
+int main() 
 {
+
   int rdnum1, rdnum2, rdnum3;
 
   /* ************************************
@@ -9,10 +11,17 @@ int main()
   And calculate the summation and average of random numbers.
     Code your program here
   ************************************ */
+  srand(static_cast<unsigned>(time(nullptr)));
+
+  rdnum1 = rand() % 100;
+  rdnum2 = rand() % 100;
+  rdnum3 = rand() % 100;
+  
+   int total = rdnum1 + rdnum2 + rdnum3;
+   double average = static_cast<double>(total) / 3.0;
 
   cout << setprecision(2) << fixed;
-  cout << "Random values are " << rdnum1 << "\t" << rdnum2 << "\t" << rdnum3 << endl;
-  cout << "Total summation: " << total << "\t"
-       << "Average "
-       << "\t" << average << endl;
+  cout << "Random values are " << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
+  cout << "Total summation: " << total << endl;
+  cout << "Average " << average << endl;
 }
